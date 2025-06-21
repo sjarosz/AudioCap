3f06fb91d76d757be240fd582f69dcafe09a4187
Steven Jarosz <>
2025-06-21 09:29:56 -0400

    hello world sdk added.

---
+++ audiosdk/audiosdk.h
@@ -0,0 +1,12 @@
+//
+//  audiosdk.h
+//  audiosdk
+//
+//  Created by Steven Jarosz (Ping) on 6/21/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface audiosdk : NSObject
+
+@end
---
+++ audiosdk/audiosdk.m
@@ -0,0 +1,12 @@
+//
+//  audiosdk.m
+//  audiosdk
+//
+//  Created by Steven Jarosz (Ping) on 6/21/25.
+//
+
+#import "audiosdk.h"
+
+@implementation audiosdk
+
+@end
